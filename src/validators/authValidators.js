1d47a75605d9be71edae643a2a641cfa131aaf83
Khushi Zanora <khushizanora1506@gmail.com>
2026-08-11 18:31:52 +0530

    emailValidate

--- src/validators/authValidators.js
+++ src/validators/authValidators.js
@@ -5,6 +5,9 @@ if (!name || name.trim().length < 2) {
     errors.push('Name must be at least 2 characters');
   }
 
-
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!email || !emailRegex.test(email)) {
+    errors.push('A valid email is required');
+  }
 
 };
