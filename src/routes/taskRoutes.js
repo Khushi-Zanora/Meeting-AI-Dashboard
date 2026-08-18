34339271f5a96f27dc0052a7be988ea77a59e8e0
Khushi Zanora <khushizanora1506@gmail.com>
2026-08-18 08:28:24 +0530

    modified

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { getTasks, updateTask, getMeetings } from '../controllers/taskController.js';
+import { getTasks, updateTask } from '../controllers/taskController.js';
 
 const router = express.Router();
 
 router.get('/tasks', getTasks);
 router.patch('/tasks/:id', updateTask);
-router.get('/meetings', getMeetings);
 
 export default router;
