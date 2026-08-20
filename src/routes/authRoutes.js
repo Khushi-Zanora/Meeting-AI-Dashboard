8483dc59fe21d05fa6e2bab4e5da04d4d7cd6d2d
Khushi Zanora <khushizanora1506@gmail.com>
2026-08-20 19:32:24 +0530

    modified

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.post('/refresh', refresh);
+router.post('/logout', logout);
 
 export default router;
